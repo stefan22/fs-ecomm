b754f5f3ef051498c92a12a57fc6c0e4650ff71b
stefan22 <stefanosemail@protonmail.com>
2022-03-03 14:30:45 +0000

    housekeeping

--- server/controllers/products.js
+++
@@ -1,8 +0,0 @@
-import { createRequire } from "module";
-import { v4 as uuidv4 } from 'uuid';
-const require = createRequire(import.meta.url);
-const products = require("../data/users.json");
-
-export const getProducts = (req, res) => {
-  res.send(products);
-};
