789abe669c9e0c46c6d46c7d6535851718d9848a
Monterrey <stefano@Monterreys-MacBook-Pro.local>
2022-03-12 19:32:29 +0000

    animations

---
+++ src/components/animations/FadeInDiv.js
@@ -0,0 +1,9 @@
+import styled, { keyframes } from 'styled-components'
+import { fadeIn } from 'react-animations';
+
+const fadeInAnimation = keyframes`${fadeIn}`;
+
+
+export const FadeInDiv = styled.div`
+    animation: 1.25s ${fadeInAnimation}
+`
