60ef2c2126ba3262a3a136eb3e0feef19a0dd6fb
Monterrey <stefano@Monterreys-MacBook-Pro.local>
2022-03-22 12:58:37 +0000

    gitignore updt
    amends prior rebase
    
    amends prior rebase

--- src/components/product-details/index.js
+++
@@ -1,6 +0,0 @@
-import ProductDetails from './ProductDetails'
-import ProductDescription from './ProductDescription'
-import ProductReviews from './ProductReviews'
-import MightAlsoLike from './MightAlsoLike'
-
-export { ProductDetails, ProductDescription, ProductReviews, MightAlsoLike }
